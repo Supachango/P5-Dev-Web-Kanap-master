dcd74dd398eab5b49a552110e60d8b9fe22a19e0
Fabien LELOUEDEC <lelouedec.fabien@gmail.com>
2023-05-04 08:48:36 +0200

    🧱 Appel d'un seul élément

--- front/js/script.js
+++ front/js/script.js
@@ -12,36 +12,17 @@ for (let i= 0; i < canapes.length; i++ ){
     //Récupération de l'élément du DOM qui acceuillera les produits
     const sectionItems = document.querySelector(".items");
 
-    // mis avant
-     //Création d'une balise article dédiée aux canapés
+    //Création d'une balise article dédiée aux canapés
      const articleElement = document.createElement("article"); 
     //Ajout du DOM : On rattache la balise article à la section "item"
     sectionItems.appendChild(articleElement);
 
 
-    //génération de la balise html img pour imageUrl
-    const imageUrlElement = document.createElement("img")
-    imageUrlElement.src = article.imageUrl;
+    //génération de la balise html img pour imageUrl déclarée imgK
+    const imgKElement = document.createElement("img")
+    imgKElement.src = article.imageUrl;
     //Ajout des éléments du DOM
-    articleElement.appendChild(imageUrlElement)
-
-    //génération de la balise html p avec la propriété alt pour apparaitre à la place de l'image. (à tester)
-    const altTextElement = document.createElement("p");
-    altTextElement.alt = article.altTxt;
-    //Ajout des éléments du DOM
-    articleElement.appendChild(altTextElement)
-
-    //génération de la balise html h3 pour name
-    const nameElement = document.createElement("h3");
-    nameElement.innerText = article.name;  
-    //Ajout des éléments du DOM
-    articleElement.appendChild(nameElement)
-
-    //génération de la balise html p pour description
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-    //Ajout des éléments du DOM
-    articleElement.appendChild(descriptionElement)
+    articleElement.appendChild(imgKElement)
 
 }
 }
