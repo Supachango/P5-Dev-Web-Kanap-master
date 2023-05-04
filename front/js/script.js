05474a8121347e3fa531a72c38808ec72439b64a
Fabien LELOUEDEC <lelouedec.fabien@gmail.com>
2023-05-04 08:18:55 +0200

    Etape 3 : JS

--- front/js/script.js
+++ front/js/script.js
@@ -3,50 +3,53 @@ async function getCanapes(){
     const reponse = await fetch('http://localhost:3000/api/products')
     const canapes = await reponse.json();
     console.log (canapes)
+
+//On entoure le code de génération par une boucle FOR
+for (let i= 0; i < canapes.length; i++ ){
+    //On crée une variable i pour récupérer le canapé à l'index du parcours de la boucle.
+    const article = canapes[i]
+
+    //Récupération de l'élément du DOM qui acceuillera les produits
+    const sectionItems = document.querySelector(".items");
+
+    // mis avant
+     //Création d'une balise article dédiée aux canapés
+     const articleElement = document.createElement("article"); 
+    //Ajout du DOM : On rattache la balise article à la section "item"
+    sectionItems.appendChild(articleElement);
+
+
+    //génération de la balise html img pour imageUrl
+    const imageUrlElement = document.createElement("img")
+    imageUrlElement.src = article.imageUrl;
+    //Ajout des éléments du DOM
+    articleElement.appendChild(imageUrlElement)
+
+    //génération de la balise html p avec la propriété alt pour apparaitre à la place de l'image. (à tester)
+    const altTextElement = document.createElement("p");
+    altTextElement.alt = article.altTxt;
+    //Ajout des éléments du DOM
+    articleElement.appendChild(altTextElement)
+
+    //génération de la balise html h3 pour name
+    const nameElement = document.createElement("h3");
+    nameElement.innerText = article.name;  
+    //Ajout des éléments du DOM
+    articleElement.appendChild(nameElement)
+
+    //génération de la balise html p pour description
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    //Ajout des éléments du DOM
+    articleElement.appendChild(descriptionElement)
+
+}
 }
 
 getCanapes()
 
-for (let i= 0; i < canapes.length; i++ ){
-    const products = canapes[i]
 
-    const sectionItems = document.querySelector(".items");
 
-    const imageElement = document.createElement('products'); 
-    
-    
-    
-    const colorsElement = document.createElement("h2");
-    colorsElement.innerText = products[c].colors;
-
-    const _idElement = document.createElement("p");
-    _idElement.innerText = products._id;
-
-    const nameElement = document.createElement("h3");
-    nameElement.innerText = products.name;
-
-    const priceElement = document.createElement("p");
-    priceElement.innerText = products.price;
-
-    const imageUrlElement = document.createElement("img");
-    imageUrlElement.src = products[i].imageUrl;
-
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = products.description ?? "Pas de description pour le moment.";
-
-    const altTextElement = document.createElement("p");
-    altTextElement.innerText = products.altText;
-
-    sectionItems.appendChild(productsElement);
-    productsElement.appendChild(colorsElement);
-    productsElement.appendChild(_idElement)
-    productsElement.appendChild(nameElement)
-    productsElement.appendChild(priceElement)
-    productsElement.appendChild(imageUrlElement)
-    productsElement.appendChild(descriptionElement)
-    productsElement.appendChild(altTextElement)
-
-}
 
     
 
